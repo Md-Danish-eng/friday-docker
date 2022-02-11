caf0f772502b82f32dbb907c16e4c1e0444987d9
root <root@ip-172-31-25-84.ec2.internal>
2022-02-11 09:30:03 +0000

    first

---
+++ main.js
@@ -0,0 +1,12 @@
+var express = require('express')    //Load express module with `require` directive
+var app = express() 
+ 
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+  res.send('Hello Welcome to Nodejs application')
+})
+ 
+ 
+app.listen(8081, function () {
+  console.log('app listening on port 8081!')
+})
